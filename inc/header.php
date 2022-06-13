a8975f1905fc7b1846dc3e06776e1539afe10d2c
Filatov Dmytro <dmitrovfilatov@gmail.com>
2022-06-13 11:53:28 +0300

    buttons

--- inc/header.php
+++ inc/header.php
@@ -35,53 +35,40 @@ $title = $isHomepage ? $siteName : $pageName . ' : ' . $siteName;
 <body class="l-body -page_<?php echo $uri; ?><?php echo (!$isHomepage) ? ' -page_inner' : ''; ?>">
 
   <div class="l-wrapper">
+
     <header class="l-siteHeader">
-      <div class="b-siteHeader">
-        <div class="l-siteLogo">
-          <?php
-          $siteLogo__tag  = $isHomepage ? 'h1' : 'div';
-          $siteLogo__link = $isHomepage ? '' : ' href="index.html"';
-          ?>
-          <<?php echo $siteLogo__tag; ?> class="b-siteLogo" itemscope itemtype="http://schema.org/Organization">
-            <a class="b-siteLogo__link" <?php echo $siteLogo__link; ?> itemprop="url">
-              <img class="b-siteLogo__icon" src="assets/img/blocks/siteLogo/siteLogo-logo.png" alt="<?php echo $siteName; ?>" title="<?php echo $siteName; ?>" itemprop="logo" />
-            </a>
-          </<?php echo $siteLogo__tag; ?>>
+      <div class="l-content">
+        <div class="b-siteHeader">
+          <div class="l-siteLogo">
+            <?php
+            $siteLogo__tag  = $isHomepage ? 'h1' : 'div';
+            $siteLogo__link = $isHomepage ? '' : ' href="index.html"';
+            ?>
+            <<?php echo $siteLogo__tag; ?> class="b-siteLogo" itemscope itemtype="http://schema.org/Organization">
+              <a class="b-siteLogo__link" <?php echo $siteLogo__link; ?> itemprop="url">
+                <img class="b-siteLogo__icon" src="assets/img/blocks/siteLogo/siteLogo-logo.png" alt="<?php echo $siteName; ?>" title="<?php echo $siteName; ?>" itemprop="logo" />
+              </a>
+            </<?php echo $siteLogo__tag; ?>>
+          </div>
+
+          <div class="l-siteSearch">
+            <form class="b-siteSearch" role="search" method="post" action="#">
+              <label class="b-siteSearch__search">
+                <span class="b-siteSearch__label g-visuallyhidden">Search:</span>
+                <input class="b-siteSearch__input" type="search" name="s" spellcheck="true" />
+              </label>
+
+              <input class="b-siteSearch__button" type="submit" value="Search" />
+            </form>
+          </div>
+
+          <nav class="l-mainNavigation">
+            <ul class="b-mainNavigation">
+              <li class="b-mainNavigation__item<?php echo ($uri == 'home') ? ' -state_active' : ''; ?>">
+                <a class="b-mainNavigation__link" href="index.html">Test Filatov programmer</a>
+              </li>
+            </ul>
+          </nav>
         </div>
-
-        <div class="l-siteSearch">
-          <form class="b-siteSearch" role="search" method="post" action="#">
-            <label class="b-siteSearch__search">
-              <span class="b-siteSearch__label g-visuallyhidden">Search:</span>
-              <input class="b-siteSearch__input" type="search" name="s" spellcheck="true" />
-            </label>
-
-            <input class="b-siteSearch__button" type="submit" value="Search" />
-          </form>
-        </div>
-
-        <nav class="l-mainNavigation">
-          <ul class="b-mainNavigation">
-            <li class="b-mainNavigation__item<?php echo ($uri == 'home') ? ' -state_active' : ''; ?>">
-              <a class="b-mainNavigation__link" href="index.html">Test Filatov programmer2</a>
-            </li>
-          </ul>
-        </nav>
       </div>
     </header>
-
-    <div class="l-content">
-      <ul class="menu-buttons">
-        <li class="buttons-items"><a class="buttons-items-link" target="_blank" href=" #">Grundlagen</a></li>
-        <li class="buttons-items"><a class="buttons-items-link" target="_blank" href=" #">Gesellschaft</a></li>
-        <li class="buttons-items"><a class="buttons-items-link" target="_blank" href=" #">Sprachen</a></li>
-        <li class="buttons-items"><a class="buttons-items-link" target="_blank" href=" #">Gestalten</a></li>
-        <li class="buttons-items"><a class="buttons-items-link" target="_blank" href=" #">Beruf</a></li>
-        <li class="buttons-items"><a class="buttons-items-link" target="_blank" href=" #">Kultur</a></li>
-        <li class="buttons-items"><a class="buttons-items-link" target="_blank" href=" #">Gemeinchaft</a></li>
-        <li class="buttons-items"><a class="buttons-items-link" target="_blank" href=" #">Natur</a></li>
-        <li class="buttons-items"><a class="buttons-items-link" target="_blank" href=" #">Gesundheit</a></li>
-        <li class="buttons-items"><a class="buttons-items-link" target="_blank" href=" #">Technik</a></li>
-      </ul>
-
-    </div>
--- index.php
+++ index.php
@@ -7,12 +7,122 @@ require('inc/header.php');
 
 <main class="l-contentText">
 
-    <section class="l-categoryList">
-        <div class="b-categoryList">
-            <p class="b-categoryList__text">Text</p>
-            <a class="b-categoryList__link" href="">Link</a>
-        </div>
-    </section>
+    <div class="l-content">
+        <ul class="menu-buttons">
+            <li class="buttons-items">
+                <a class="buttons-items-link" target="_blank" href="#">
+
+                    <svg class="regular-customers-svg">
+                        <use href="assets/img/sprites/icons.svg#icon-facebook" width="28" height="28">
+                        </use>
+                    </svg>
+
+                    <p> Grundlagen</p>
+                </a>
+            </li>
+            <li class="buttons-items">
+                <a class="buttons-items-link" target="_blank" href="#">
+
+                    <svg class="regular-customers-svg">
+                        <use href="assets/img/sprites/icons.svg#icon-facebook" width="28" height="28">
+                        </use>
+                    </svg>
+
+                    <p>Gesellschaft</p>
+                </a>
+            </li>
+            <li class="buttons-items">
+                <a class="buttons-items-link" target="_blank" href="#">
+
+                    <svg class="regular-customers-svg">
+                        <use href="assets/img/sprites/icons.svg#icon-facebook" width="28" height="28">
+                        </use>
+                    </svg>
+
+                    <p>Sprachen</p>
+                </a>
+            </li>
+            <li class="buttons-items">
+                <a class="buttons-items-link" target="_blank" href="#">
+
+                    <svg class="regular-customers-svg">
+                        <use href="assets/img/sprites/icons.svg#icon-facebook" width="28" height="28">
+                        </use>
+                    </svg>
+
+                    <p>Gestalten</p>
+                </a>
+            </li>
+            <li class="buttons-items">
+                <a class="buttons-items-link" target="_blank" href="#">
+
+                    <svg class="regular-customers-svg">
+                        <use href="assets/img/sprites/icons.svg#icon-facebook" width="28" height="28">
+                        </use>
+                    </svg>
+
+                    <p>Beruf</p>
+                </a>
+            </li>
+            <li class="buttons-items">
+                <a class="buttons-items-link" target="_blank" href="#">
+
+                    <svg class="regular-customers-svg">
+                        <use href="assets/img/sprites/icons.svg#icon-facebook" width="28" height="28">
+                        </use>
+                    </svg>
+
+                    <p>Kultur</p>
+                </a>
+            </li>
+            <li class="buttons-items">
+                <a class="buttons-items-link" target="_blank" href="#">
+
+                    <svg class="regular-customers-svg">
+                        <use href="assets/img/sprites/icons.svg#icon-facebook" width="28" height="28">
+                        </use>
+                    </svg>
+
+                    <p>Gemeinchaft</p>
+                </a>
+            </li>
+            <li class="buttons-items">
+                <a class="buttons-items-link" target="_blank" href="#">
+
+                    <svg class="regular-customers-svg">
+                        <use href="assets/img/sprites/icons.svg#icon-facebook" width="28" height="28">
+                        </use>
+                    </svg>
+
+                    <p>Natur</p>
+                </a>
+            </li>
+            <li class="buttons-items">
+                <a class="buttons-items-link" target="_blank" href="#">
+
+                    <svg class="regular-customers-svg">
+                        <use href="assets/img/sprites/icons.svg#icon-facebook" width="28" height="28">
+                        </use>
+                    </svg>
+
+                    <p>Gesundheit</p>
+                </a>
+            </li>
+            <li class="buttons-items">
+                <a class="buttons-items-link" target="_blank" href="#">
+
+                    <svg class="regular-customers-svg">
+                        <use href="assets/img/sprites/icons.svg#icon-facebook" width="28" height="28">
+                        </use>
+                    </svg>
+
+                    <p>Technik</p>
+                </a>
+            </li>
+
+        </ul>
+    </div>
+
 </main>
 
 <?php require('inc/footer.php'); ?>
