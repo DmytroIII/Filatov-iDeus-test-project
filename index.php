b289f6360d3e069ae5d176c73b780ba3fea512ea
Filatov Dmytro <dmitrovfilatov@gmail.com>
2022-06-13 14:14:54 +0300

    add 3 blocks

--- index.php
+++ index.php
@@ -6,72 +6,59 @@ require('inc/header.php');
 ?>
 
 <main class="l-contentText">
-
     <div class="l-content">
         <ul class="menu-buttons">
             <li class="buttons-items">
                 <a class="buttons-items-link" target="_blank" href="#">
-
                     <svg class="regular-customers-svg">
                         <use href="assets/img/sprites/icons.svg#icon-facebook" width="28" height="28">
                         </use>
                     </svg>
-
                     <p> Grundlagen</p>
                 </a>
             </li>
             <li class="buttons-items">
                 <a class="buttons-items-link" target="_blank" href="#">
-
                     <svg class="regular-customers-svg">
                         <use href="assets/img/sprites/icons.svg#icon-facebook" width="28" height="28">
                         </use>
                     </svg>
-
                     <p>Gesellschaft</p>
                 </a>
             </li>
             <li class="buttons-items">
                 <a class="buttons-items-link" target="_blank" href="#">
-
                     <svg class="regular-customers-svg">
                         <use href="assets/img/sprites/icons.svg#icon-facebook" width="28" height="28">
                         </use>
                     </svg>
-
                     <p>Sprachen</p>
                 </a>
             </li>
             <li class="buttons-items">
                 <a class="buttons-items-link" target="_blank" href="#">
-
                     <svg class="regular-customers-svg">
                         <use href="assets/img/sprites/icons.svg#icon-facebook" width="28" height="28">
                         </use>
                     </svg>
-
                     <p>Gestalten</p>
                 </a>
             </li>
             <li class="buttons-items">
                 <a class="buttons-items-link" target="_blank" href="#">
-
                     <svg class="regular-customers-svg">
                         <use href="assets/img/sprites/icons.svg#icon-facebook" width="28" height="28">
                         </use>
                     </svg>
-
                     <p>Beruf</p>
                 </a>
             </li>
             <li class="buttons-items">
                 <a class="buttons-items-link" target="_blank" href="#">
-
                     <svg class="regular-customers-svg">
                         <use href="assets/img/sprites/icons.svg#icon-facebook" width="28" height="28">
                         </use>
                     </svg>
-
                     <p>Kultur</p>
                 </a>
             </li>
@@ -82,47 +69,57 @@ require('inc/header.php');
                         <use href="assets/img/sprites/icons.svg#icon-facebook" width="28" height="28">
                         </use>
                     </svg>
-
                     <p>Gemeinchaft</p>
                 </a>
             </li>
             <li class="buttons-items">
                 <a class="buttons-items-link" target="_blank" href="#">
-
                     <svg class="regular-customers-svg">
                         <use href="assets/img/sprites/icons.svg#icon-facebook" width="28" height="28">
                         </use>
                     </svg>
-
                     <p>Natur</p>
                 </a>
             </li>
             <li class="buttons-items">
                 <a class="buttons-items-link" target="_blank" href="#">
-
                     <svg class="regular-customers-svg">
                         <use href="assets/img/sprites/icons.svg#icon-facebook" width="28" height="28">
                         </use>
                     </svg>
-
                     <p>Gesundheit</p>
                 </a>
             </li>
             <li class="buttons-items">
                 <a class="buttons-items-link" target="_blank" href="#">
-
                     <svg class="regular-customers-svg">
                         <use href="assets/img/sprites/icons.svg#icon-facebook" width="28" height="28">
                         </use>
                     </svg>
-
                     <p>Technik</p>
                 </a>
             </li>
-
         </ul>
     </div>
+    <div class="l-content">
+        <div class="b-developerCopyright">
+            <div class=" b-siteArticles">
+                <div class="b-siteLeftColumn">
+                    <h2>Kommende Veranstaltungen</h2>
+                    <ul class="b-siteTextArticles">
+                        <li>Der Kurs findet 2 × wöchentlich statt und erlaubt Ihnen in einem semi-intensiven Lerntempo voranzukommen. Nut...</li>
+                    </ul>
+                </div>
 
+                <div class="b-siteRightColumn">
+                    <h2>Mitteilungen</h2>
+                    <ul class="b-siteTextArticles">
+                        <li>Der Kurs findet 2 × wöchentlich statt und erlaubt Ihnen in einem semi-intensiven Lerntempo voranzukommen. Nut...</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
 
 <?php require('inc/footer.php'); ?>
