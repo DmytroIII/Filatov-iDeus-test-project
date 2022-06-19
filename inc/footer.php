f602f0adfdc2ff0682bb13efc2caa330c0587dbd
Filatov Dmytro <dmitrovfilatov@gmail.com>
2022-06-20 00:35:25 +0300

    commit 20.06 night

--- inc/footer.php
+++ inc/footer.php
@@ -1,13 +1,40 @@
   <!--content-->
 
-  <footer class="l-siteFooter">
-    <div class="footer">Подвал</div>
+  <footer class="b-siteFooter">
+    <div class="l-siteFooterBlock">
+      <div class="b-siteAdress">
+        <h3 class="b-siteAdress_h3">Adresse</h3>
+        <div class="b-siteAdress_location">
+          VSV - AUPS </br>
+          Bärengasse 22</br>
+          8001 Zürich
+        </div>
+      </div>
+      <div class="b-siteContacts">
+        <h3 class="b-siteContacts__h3">Kontakt</h3>
+        <div class="b-siteContacts__mail">office@vhs.ch</div>
+        <div class="b-siteContacts__phone"> +41 44 211 97 71</div>
+        <div class="b-siteContacts__fax">+41 44 211 97 72</div>
+      </div>
+      <div class="b-siteQuickLinks">
+        <h3 class="b-siteQuickLinks__h3">Quick Links</h3>
+        <div class="b-siteQuickLinks__link">Mitglied werden</div>
+        <div class="b-siteQuickLinks__link">Statuten</div>
+        <div class="b-siteQuickLinks__link">Newsletter abonnieren</div>
+      </div>
+    </div>
+    <div class="b-siteCopyright">
+      <span class="b-siteCopyright__BoldText">
+        © COPYRIGHT 2021. ALL RIGHTS RESERVED | </span>
+      IMPRESSUM
+    </div>
   </footer>
 
 
   <!--wrapper-->
 
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous">
+  </script>
   <script>
     window.jQuery || document.write('<script src="assets/js/vendor/jquery-3.6.0.min.js"><\/script>')
   </script>
--- index.php
+++ index.php
@@ -16,80 +16,80 @@ require('inc/header.php');
 
                 <li class="b-contentCategories__list">
                     <a href="#" class="b-contentCategories__link">
-                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/atomic-structure.png" width="28" height="28" />
-                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/atomic-structure-hover.png" width="28" height="28" />
+                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/atomic-structure.png" width="28" height="28" alt="atomic structure" />
+                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/atomic-structure-hover.png" width="28" height="28" alt="atomic structure" />
                         <span class="b-contentCategories__text">Grundlagen</span>
                     </a>
                 </li>
 
                 <li class="b-contentCategories__list">
                     <a href="#" class="b-contentCategories__link">
-                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/group.png" width="28" height="28" />
-                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/group_hover.png" width="28" height="28" />
+                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/group.png" width="28" height="28" alt="group" />
+                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/group_hover.png" width="28" height="28" alt="group" />
                         <span class="b-contentCategories__text">Gesellschaft</span>
                     </a>
                 </li>
 
                 <li class="b-contentCategories__list">
                     <a href="#" class="b-contentCategories__link">
-                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/chat.png" width="28" height="28" />
-                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/chatHover.png" width="28" height="28" />
+                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/chat.png" width="28" height="28" alt="chat" />
+                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/chatHover.png" width="28" height="28" alt="chat" />
                         <span class="b-contentCategories__text">Sprachen</span>
                     </a>
                 </li>
 
                 <li class="b-contentCategories__list">
                     <a href="#" class="b-contentCategories__link">
-                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/paintPng.png" width="28" height="28" />
-                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/paintPngHover.png" width="28" height="28" />
+                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/paintPng.png" width="28" height="28" alt="paint" />
+                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/paintPngHover.png" width="28" height="28" alt="paint" />
                         <span class="b-contentCategories__text">Gestalten</span>
                     </a>
                 </li>
 
                 <li class="b-contentCategories__list">
                     <a href="#" class="b-contentCategories__link">
-                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/casePNG.png" width="28" height="28" />
-                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/casePNGHover.png" width="28" height="28" />
+                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/casePNG.png" width="28" height="28" alt="case" />
+                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/casePNGHover.png" width="28" height="28" alt="case" />
                         <span class="b-contentCategories__text">Beruf</span>
                     </a>
                 </li>
 
                 <li class="b-contentCategories__list">
                     <a href="#" class="b-contentCategories__link">
-                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/bank.png" width="28" height="28" />
-                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/bunkhover.png" width="28" height="28" />
+                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/bank.png" width="28" height="28" alt="bank" />
+                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/bunkhover.png" width="28" height="28" alt="bank" />
                         <span class="b-contentCategories__text">Kultur</span>
                     </a>
                 </li>
 
                 <li class="b-contentCategories__list">
                     <a href="#" class="b-contentCategories__link">
-                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/eartn.png" width="28" height="28" />
-                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/eartnHover.png" width="28" height="28" />
+                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/eartn.png" width="28" height="28" alt="eartn" />
+                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/eartnHover.png" width="28" height="28" alt="eartn" />
                         <span class="b-contentCategories__text">Gemeinschaft</span>
                     </a>
                 </li>
 
                 <li class="b-contentCategories__list">
                     <a href="#" class="b-contentCategories__link">
-                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/leaf.png" width="28" height="28" />
-                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/leafhover.png" width="28" height="28" />
+                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/leaf.png" width="28" height="28" alt="leaf" />
+                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/leafhover.png" width="28" height="28" alt="leaf" />
                         <span class="b-contentCategories__text">Natur</span>
                     </a>
                 </li>
 
                 <li class="b-contentCategories__list">
                     <a href="#" class="b-contentCategories__link">
-                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/hurt.png" width="28" height="28" />
-                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/hurtHover.png" width="28" height="28" />
+                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/hurt.png" width="28" height="28" alt="hurt" />
+                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/hurtHover.png" width="28" height="28" alt="hurt" />
                         <span class="b-contentCategories__text">Gesundheit</span>
                     </a>
                 </li>
 
                 <li class="b-contentCategories__list">
                     <a href="#" class="b-contentCategories__link">
-                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/gear.png" width="28" height="28" />
-                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/gearHover.png" width="28" height="28" />
+                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/gear.png" width="28" height="28" alt="gear" />
+                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/gearHover.png" width="28" height="28" alt="gear" />
                         <span class="b-contentCategories__text">Technik</span>
                     </a>
                 </li>
@@ -97,13 +97,51 @@ require('inc/header.php');
             </ul>
 
             <div class="l-contentNews">
-                <div class="b-contentNews">
-                    <a href="" class="b-contentNews__link"></a>
-                    <a href="" class="b-contentNews__link"></a>
+
+                <div class="l-contentLeftColumn">
+                    <h2 class="l-contentNews_h2">Kommende Veranstaltungen</h2>
+
+                    <div class="b-contentEvents">
+                        <span class="b-contentEvents__data">Di 19.10.2021 08:30</span>
+                        <h3 class="b-contentEvents__h3">214-41711 Deutsch als Fremdsprache B2/1 - Semesterkurs</h3>
+                        <div class="b-contentEvents__author">Referent/in: Elvira Canova
+                            Volkshochschule Bern</div>
+                        <div class="b-contentEvents__text">Der Kurs findet 2 x wöchentlich statt und erlaubt Ihnen in einem semi-intensiven Lerntempo voranzukommen. Nut...</div>
+                    </div>
+
+                    <div class="b-contentEvents">
+                        <span class="b-contentEvents__data">Di 19.10.2021 08:30</span>
+                        <h3 class="b-contentEvents__h3">214-41711 Deutsch als Fremdsprache B2/1 - Semesterkurs</h3>
+                        <div class="b-contentEvents__author">Referent/in: Elvira Canova
+                            Volkshochschule Bern</div>
+                        <div class="b-contentEvents__text">Der Kurs findet 2 x wöchentlich statt und erlaubt Ihnen in einem semi-intensiven Lerntempo voranzukommen. Nut...</div>
+                    </div>
+
+                </div>
+
+
+
+                <div class="l-contentRightColumn">
+                    <h2 class="l-contentRightColumn_h2">Mitteilungen</h2>
+
+                    <div class="b-contentArticles">
+                        <img class="b-contentArticles__img" src="assets/img/blocks/articlesImage/CoronaSieg.jpg" alt="Corona Sieg" width="515" height="158" />
+                        <h3 class="b-contentArticles__h3">Corona - neue Normalität </h3>
+                        <span class="b-contentArticles__data">24.06.2020</span>
+                        <div class="b-contentArticles__text">Am 19. Juni hat der Bundesrat weitgehende Lockerungen beschlossen, die vor allem den Weiterbildungsbereich betreffen. Damit kehrt an vielen Schulen so…</div>
+                    </div>
+
+                    <div class="b-contentArticles">
+                        <img class="b-contentArticles__img" src="assets/img/blocks/articlesImage/Coroanavirus.jpg" alt="Corona Sieg" width="515" height="158" />
+                        <h2 class="b-contentArticles__h3"> </h2>
+                        <span class="b-contentArticles__data"></span>
+                        <div class="b-contentArticles__text">Am 19. Juni hat der Bundesrat weitgehende Lockerungen beschlossen, die vor allem den Weiterbildungsbereich betreffen. Damit kehrt an vielen Schulen so…</div>
+                    </div>
 
                 </div>
 
             </div>
+        </div>
     </main>
 </div>
 <?php require('inc/footer.php'); ?>
