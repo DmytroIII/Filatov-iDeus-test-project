b7f952877a99e06457b069b6dea6199e5a0d62a1
Filatov Dmytro <dmitrovfilatov@gmail.com>
2022-06-19 18:35:20 +0300

    svg

--- index.php
+++ index.php
@@ -16,8 +16,8 @@ require('inc/header.php');
 
                 <li class="b-contentCategories__list">
                     <a href="#" class="b-contentCategories__link">
-                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/atomic-structure-q.png" width="28" height="28" />
-                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/automic-structure-hover.png" width="28" height="28" />
+                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/atomic-structure.png" width="28" height="28" />
+                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/atomic-structure-hover.png" width="28" height="28" />
                         <span class="b-contentCategories__text">Grundlagen</span>
                     </a>
                 </li>
@@ -32,64 +32,64 @@ require('inc/header.php');
 
                 <li class="b-contentCategories__list">
                     <a href="#" class="b-contentCategories__link">
-                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/group.png" width="28" height="28" />
-                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/group_hover.png" width="28" height="28" />
+                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/chat.png" width="28" height="28" />
+                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/chatHover.png" width="28" height="28" />
                         <span class="b-contentCategories__text">Sprachen</span>
                     </a>
                 </li>
 
                 <li class="b-contentCategories__list">
                     <a href="#" class="b-contentCategories__link">
-                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/group.png" width="28" height="28" />
-                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/group_hover.png" width="28" height="28" />
+                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/paintPng.png" width="28" height="28" />
+                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/paintPngHover.png" width="28" height="28" />
                         <span class="b-contentCategories__text">Gestalten</span>
                     </a>
                 </li>
 
                 <li class="b-contentCategories__list">
                     <a href="#" class="b-contentCategories__link">
-                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/group.png" width="28" height="28" />
-                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/group_hover.png" width="28" height="28" />
+                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/casePNG.png" width="28" height="28" />
+                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/casePNGHover.png" width="28" height="28" />
                         <span class="b-contentCategories__text">Beruf</span>
                     </a>
                 </li>
 
                 <li class="b-contentCategories__list">
                     <a href="#" class="b-contentCategories__link">
-                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/group.png" width="28" height="28" />
-                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/group_hover.png" width="28" height="28" />
+                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/bank.png" width="28" height="28" />
+                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/bankHover.png" width="28" height="28" />
                         <span class="b-contentCategories__text">Kultur</span>
                     </a>
                 </li>
 
                 <li class="b-contentCategories__list">
                     <a href="#" class="b-contentCategories__link">
-                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/group.png" width="28" height="28" />
-                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/group_hover.png" width="28" height="28" />
+                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/eartn.png" width="28" height="28" />
+                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/eartnHover.png" width="28" height="28" />
                         <span class="b-contentCategories__text">Gemeinschaft</span>
                     </a>
                 </li>
 
                 <li class="b-contentCategories__list">
                     <a href="#" class="b-contentCategories__link">
-                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/group.png" width="28" height="28" />
-                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/group_hover.png" width="28" height="28" />
+                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/leaf.png" width="28" height="28" />
+                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/leafhover.png" width="28" height="28" />
                         <span class="b-contentCategories__text">Natur</span>
                     </a>
                 </li>
 
                 <li class="b-contentCategories__list">
                     <a href="#" class="b-contentCategories__link">
-                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/group.png" width="28" height="28" />
-                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/group_hover.png" width="28" height="28" />
+                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/hurt.png" width="28" height="28" />
+                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/hurtHover.png" width="28" height="28" />
                         <span class="b-contentCategories__text">Gesundheit</span>
                     </a>
                 </li>
 
                 <li class="b-contentCategories__list">
                     <a href="#" class="b-contentCategories__link">
-                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/group.png" width="28" height="28" />
-                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/group_hover.png" width="28" height="28" />
+                        <img class="b-contentCategories__img" src="assets/img/blocks/icons/gear.png" width="28" height="28" />
+                        <img class="b-contentCategories__imgHover" src="assets/img/blocks/icons/gearHover.png" width="28" height="28" />
                         <span class="b-contentCategories__text">Technik</span>
                     </a>
                 </li>
