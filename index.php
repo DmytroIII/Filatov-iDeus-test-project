def20f4e0136dff35fb930c0055a44628b2e3eeb
Filatov Dmytro <dmitrovfilatov@gmail.com>
2022-06-19 17:54:50 +0300

    commit

--- index.php
+++ index.php
@@ -2,11 +2,24 @@
 $pageName = 'Home';
 $siteName = '%siteName%';
 $uri      = 'home';
-require( 'inc/header.php' );
+require('inc/header.php');
 ?>
 
 <main class="l-contentText">
 
+
+    <ul class="item"></ul>
+
+    <ul class="item"></ul>
+    <ul class="item"></ul>
+    <ul class="item"></ul>
+    <ul class="item"></ul>
+    <ul class="item"></ul>
+    <ul class="item"></ul>
+    <ul class="item"></ul>
+    <ul class="item"></ul>
+
+    <div class="items"></div>
 </main>
 
-<?php require( 'inc/footer.php' ); ?>
+<?php require('inc/footer.php'); ?>
